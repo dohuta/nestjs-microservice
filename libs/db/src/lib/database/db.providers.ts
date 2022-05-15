9b1df15033134fd478f651fb9ed92855109d801c
Tap Huynh <thuynh68@dxc.com>
2022-05-15 11:37:54 +0700

    Change connection string from param declaration to connection string

--- libs/db/src/lib/database/db.providers.ts
+++ libs/db/src/lib/database/db.providers.ts
@@ -1,19 +1,20 @@
 import { Sequelize } from 'sequelize-typescript';
-import { Note, Token, User } from '../model/model.module';
+import { Note, Token, User } from '@libs/db';
 
 export const databaseProviders = [
   {
     provide: 'SEQUELIZE',
     useFactory: async () => {
-      const sequelize = new Sequelize({
-        dialect: 'mysql',
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT || 3306),
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        ssl: true,
-      });
+      // const sequelize = new Sequelize({
+      //   dialect: 'mysql',
+      //   host: process.env.DB_HOST,
+      //   port: Number(process.env.DB_PORT || 3306),
+      //   username: process.env.DB_USER,
+      //   password: process.env.DB_PASS,
+      //   database: process.env.DB_NAME,
+      //   ssl: true,
+      // });
+      const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING || '');
       sequelize.addModels([User, Token, Note]);
       try {
         await sequelize.sync();
